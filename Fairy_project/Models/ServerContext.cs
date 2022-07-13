b1feb71173199a94a9ce804ed32f656b059047ee
viss5vuus98 <dokipu@gmailcom>
2022-07-13 09:16:07 +0800

    Merge

--- Fairy_project/Models/ServerContext.cs
+++ Fairy_project/Models/ServerContext.cs
@@ -8,7 +8,7 @@ namespace Fairy_project.Models
         public ServerContext(DbContextOptions<ServerContext> options) : base(options) { }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=tcp:wo7473.database.windows.net,1433;Initial Catalog=woo;Persist Security Info=False;User ID=ispanwo@086@wo7473;Password=p@ssw0rd-iii;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            optionsBuilder.UseSqlServer("Server=tcp:wo7473.database.windows.net,1433;Initial Catalog=wow;Persist Security Info=False;User ID=ispanwo@086@wo7473;Password=p@ssw0rd-iii;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         }
 
         public DbSet<Exhibition> exhibitions { get; set; }
@@ -17,5 +17,9 @@ namespace Fairy_project.Models
         public DbSet<Permissions> Permissions { get; set; }
         public DbSet<Ticket> tickets { get; set; }
         public DbSet<Booth> booths { get; set; }
+        public DbSet<Apply> Applies { get; set; }
+        public DbSet<Area> areas { get; set; }
+        public DbSet<BoothMap> boothMaps { get; set; }
+        public DbSet<Manager> managers { get; set; }
     }
 }
