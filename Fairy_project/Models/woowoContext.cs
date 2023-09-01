08398bec6033dd6a1a94b7db4d5f74f10e71591d
pohan chen <a0931771160@gmail.com>
2023-09-01 16:41:37 +0800

    first commit

--- Fairy_project/Models/woowoContext.cs
+++ Fairy_project/Models/woowoContext.cs
@@ -31,7 +31,8 @@ namespace Fairy_project.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=tcp:pohaniii.database.windows.net,1433;Initial Catalog=woowo;Persist Security Info=False;User ID=ispan;Password=P@ssw0rd-iii;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                //optionsBuilder.UseSqlServer("Server=tcp:pohaniii.database.windows.net,1433;Initial Catalog=woowo;Persist Security Info=False;User ID=ispan;Password=P@ssw0rd-iii;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                optionsBuilder.UseSqlServer("Server=.;Initial Catalog=woowo;Integrated Security=SSPI;Trusted_Connection=Yes;");
             }
         }
 
